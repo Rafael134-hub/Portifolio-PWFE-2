c46f698eaabea870ea470e2e4272027137702d59
Rafael34-hub <rafael.barbosa.moreira24@gmail.com>
2025-03-31 01:17:18 -0300

    'version13'

---
+++ scripts/home/home.js
@@ -0,0 +1,5 @@
+let button_state = document.getElementById("home_button");
+
+button_state.addEventListener("click", () => {
+    window.open('https://www.figma.com/design/PyStvJpKQUjN2xJnXEiP57/JetReady?t=imhppbZa83QTrNeP-1', '_blank');
+});
