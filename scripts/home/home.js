5cad74e12495d5e33f29d28328bd223f78a0fc56
Rafael Barbosa Moreira <56703342806@>
2025-03-31 16:58:51 -0300

    'version15'

--- scripts/home/home.js
+++ scripts/home/home.js
@@ -1,5 +1,48 @@
-let button_state = document.getElementById("home_button");
+let fundo_da_imagem = document.querySelector(".home_main_section");
+let container_fundo = document.querySelector(".main_home_container");
 
-button_state.addEventListener("click", () => {
-    window.open('https://www.figma.com/design/PyStvJpKQUjN2xJnXEiP57/JetReady?t=imhppbZa83QTrNeP-1', '_blank');
+let class_icon_1 = document.getElementById("hime_fundo");
+let class_icon_2 = document.getElementById("anya_fundo");
+let class_icon_3 = document.getElementById("luffy_fundo");
+let class_icon_4 = document.getElementById("frieren_fundo");
+
+class_icon_1.addEventListener("mouseover", () => {
+    fundo_da_imagem.style.backgroundImage = "radial-gradient(circle, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7) 0%), url('images/hime_fundo.png')";
+    container_fundo.style.transition = "all 1s ease-in-out";
+});
+
+class_icon_2.addEventListener("mouseover", () => {
+    fundo_da_imagem.style.background = "radial-gradient(circle, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7) 0%), url('images/fundo_anya.png')";
+    container_fundo.style.transition = "all 1s ease-in-out";
+});
+
+class_icon_3.addEventListener("mouseover", () => {
+    fundo_da_imagem.style.backgroundImage = "radial-gradient(circle, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7) 0%), url('images/luffy_fundo.jpg')";
+    container_fundo.transition = "all 1s ease-in-out";
+});
+
+class_icon_4.addEventListener("mouseover", () => {
+    fundo_da_imagem.style.backgroundImage = "radial-gradient(circle, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7) 0%), url('images/frieren_fundo.jpg')";
+    container_fundo.style.transition = "all 1s ease-in-out";
+});
+
+
+class_icon_1.addEventListener("mouseout", () => {
+    fundo_da_imagem.style.backgroundImage = "radial-gradient(circle, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7) 0%), url('images/hime_fundo.png')";
+    container_fundo.style.transition = "all 1s ease-in-out";
+});
+
+class_icon_2.addEventListener("mouseout", () => {
+    fundo_da_imagem.style.backgroundImage = "radial-gradient(circle, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7) 0%), url('images/hime_fundo.png')";
+    container_fundo.style.transition = "all 1s ease-in-out";
+});
+
+class_icon_3.addEventListener("mouseout", () => {
+    fundo_da_imagem.style.backgroundImage = "radial-gradient(circle, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7) 0%), url('images/hime_fundo.png')";
+    container_fundo.style.transition = "all 1s ease-in-out";
+});
+
+class_icon_4.addEventListener("mouseout", () => {
+    fundo_da_imagem.style.backgroundImage = "radial-gradient(circle, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7) 0%), url('images/hime_fundo.png')";
+    container_fundo.style.transition = "all 1s ease-in-out";
 });
